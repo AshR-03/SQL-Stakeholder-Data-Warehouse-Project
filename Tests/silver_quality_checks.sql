f17bee779a47ab07e679260e0dd975993ac7a76b
Ashley <135636209+AshR-03@users.noreply.github.com>
2025-02-19 22:32:22 +0000

    Update silver_quality_checks.sql

--- Tests/silver_quality_checks.sql
+++ Tests/silver_quality_checks.sql
@@ -1,4 +1,13 @@
--- Detect quality issues within the bronze data for the silver layer
+/*
+---------------------------------------
+This script tests the quality of the bronze data for each table in the schema.
+- Tests for Nulls, Incomplete values, Whitespace, incorrect counts/lengths, Duplicate keys, Patterns and more.
+- Used to build and test the silver layer cleansing script.
+
+Usage
+- run one query at a time to test a specific unit case.
+---------------------------------------
+*/
 
 ---------------------------------------
 -- crm_cst_info
@@ -86,3 +95,91 @@ SELECT
 *
 FROM bronze.crm_prd_info
 WHERE prd_key = 'CL-CA-CA-1098'
+
+---------------------------------------
+-- crm_sales_details
+---------------------------------------
+SELECT * FROM bronze.crm_sales_details
+
+-- SLS_ORD_NUM, SLS_PRD_KEY, SLS_CUST_ID
+-- Check that they are joinable between tables as shown in the Data Integration Diagram.
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info);
+
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info);
+
+-- SLS_ORDER_DT, SLS_SHIP_DT, SLS_DUE_DT
+-- Check if the order date is less than the shipping or arrival date.
+-- Check if the dates are in range and also correct length.
+-- Predicted result : No data
+-- Actual result : Columns with 0 and length < 8 in the sls_order_dt
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt 
+OR LEN(sls_order_dt) != 8 OR sls_order_dt > '20500101' OR sls_order_dt < '19000101'
+
+-- SLS_SALES, SLS_QUANITITY, SLS_PRICE
+-- Check whether there are negatives in any of the columns, and also where price * quantity does not equal sales
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL or sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 or sls_quantity <= 0 OR sls_price <= 0
+
+
+---------------------------------------
+-- erp_cust_az12
+---------------------------------------
+
+SELECT * FROM bronze.erp_cust_az12
+SELECT * FROM silver.crm_cust_info
+
+-- CID
+-- Check that the CID matches with the cst_key from silver.crm_cust_info so tables can be merged later on.
+-- Predicted result : No data
+-- Actual result : All cids with NASA substring.
+SELECT cid FROM bronze.erp_cust_az12
+WHERE cid NOT IN (SELECT cst_key FROM silver.crm_cust_info)
+
+-- BDATE
+-- Check for extreme ranges of birth dates.
+-- Predicted result : No data
+-- Actual result : Birthdates out of extreme ranges.
+SELECT * FROM bronze.erp_cust_az12
+WHERE bdate < '1920-01-01' OR bdate > '2025-02-19'
+
+-- GEN
+-- Check low cardinality column for null / Incorrect values
+-- Predicted result : Male, Female, N/A. 
+-- Actual result: NULL, "", M, F included.
+SELECT DISTINCT gen FROM bronze.erp_cust_az12;
+
+
+---------------------------------------
+-- erp_loc_a101
+---------------------------------------
+
+SELECT * FROM bronze.erp_loc_a101
+
+-- CID
+-- Must match with the cst_key from silver.crm_cust_info
+-- Expected value : No data
+-- Actual value : All data, invalid ID format.
+SELECT cid FROM bronze.erp_loc_a101
+WHERE cid NOT IN (SELECT cst_key FROM silver.crm_cust_info)
+
+-- CNTRY
+-- Low cardinality, check for abreviations, nulls and nothing values.
+-- Predicted result : Only 1 name per country.
+-- Actual result : Multiple names per country.
+SELECT DISTINCT cntry FROM bronze.erp_loc_a101
+
+-- Check correct Distinct values
+SELECT DISTINCT cntry FROM (SELECT
+REPLACE(cid, '-', '') AS cid,
+CASE
+WHEN cntry = 'DE' THEN 'Germany'
+WHEN cntry = 'USA' OR cntry = 'US' THEN 'United States'
+WHEN cntry IS NULL OR cntry = '' THEN 'N/A'
+ELSE cntry
+END AS cntry
+FROM bronze.erp_loc_a101) AS tbl

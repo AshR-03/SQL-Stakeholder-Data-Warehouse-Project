2010c2492b4bd775c5f5803e8a27a6e66cafb8f8
Ashley <135636209+AshR-03@users.noreply.github.com>
2025-02-22 17:01:36 +0000

    Create gold_quality_checks.sql

---
+++ Tests/gold_quality_checks.sql
@@ -0,0 +1,9 @@
+-- GOLD LAYER QUALITY CHECKS
+
+------------------------------------------
+-- CUSTOMER DIMENTION VIEW CHECKS
+------------------------------------------
+
+SELECT * FROM gold.dim_customers
+
+SELECT DISTINCT gender FROM gold.dim_customers

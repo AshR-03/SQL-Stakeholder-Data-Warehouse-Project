a306ad8f611f849256e2472c861ec929a974d74b
Ashley <135636209+AshR-03@users.noreply.github.com>
2025-02-22 20:19:03 +0000

    Update gold_quality_checks.sql

--- Tests/gold_quality_checks.sql
+++ Tests/gold_quality_checks.sql
@@ -1,4 +1,8 @@
--- GOLD LAYER QUALITY CHECKS
+/*
+------------------------------------------
+This script was used to complete the quality checks for the dimentions and facts of the gold layer in the data warehouse.
+------------------------------------------
+*/
 
 ------------------------------------------
 -- CUSTOMER DIMENTION VIEW CHECKS
@@ -7,3 +11,14 @@
 SELECT * FROM gold.dim_customers
 
 SELECT DISTINCT gender FROM gold.dim_customers
+
+------------------------------------------
+-- SALES FACT VIEW CHECKS
+------------------------------------------
+
+SELECT * FROM gold.fact_sales f 
+LEFT JOIN gold.dim_customers c
+ON f.customer_id = c.customer_id
+LEFT JOIN gold.dim_products p
+ON f.product_key = p.product_key
+WHERE p.product_key IS NULL
